05f32c68cc173df4e377fc4f826ac3a3f9ba2f99
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-02-28 10:04:20 -0500

    add component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
-    /*return (
+    return (
       <div className="App">
        <h1>Hi I am a React</h1>
+        <Person/>
       </div>
-    );*/
+    );
 
-    return React.createElement('div',{className:'App'},React.createElement('h1',null,'This is test Tutorial for React.js'));
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'This is test Tutorial for React.js'));
   }
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () =>{
+    return <p>this is App Child</p>
+} 
+
+export default person
